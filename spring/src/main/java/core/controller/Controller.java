ebb9119059d8c1cbe07b920fbcd20f07778139f5
gignx <gignx12@gmail.com>
2021-12-12 18:17:47 +0000

    creating a spring project

---
+++ spring/src/main/java/core/controller/Controller.java
@@ -0,0 +1,22 @@
+package core.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class Controller {
+
+	@RequestMapping(value="/test" ,method= RequestMethod.GET)
+    public ResponseEntity<String> find() {
+        try {
+            return new ResponseEntity<>("", HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+	
+}
