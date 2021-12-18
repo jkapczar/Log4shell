39e3cf4ca066bdb66e00ca8f0ecb0af33e671bdb
gignx <gignx12@gmail.com>
2021-12-18 16:55:59 +0000

    refactore

--- spring/src/main/java/core/App.java
+++ spring/src/main/java/core/App.java
@@ -1,13 +1,27 @@
 package core;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class App {
+import core.controller.Controller;
 
+@SpringBootApplication
+public class App implements CommandLineRunner{
+	
+	static Logger logger = LogManager.getLogger(App.class);
+	
 	public static void main(String[] args) {
+		
 		SpringApplication.run(App.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.setProperty("com.sun.jndi.ldap.object.trustURLCodebase","true");
+        logger.error(System.getProperty("com.sun.jndi.ldap.object.trustURLCodebase"));
 	}
 
 }
--- spring/src/main/java/core/controller/Controller.java
+++ spring/src/main/java/core/controller/Controller.java
@@ -1,7 +1,11 @@
 package core.controller;
 
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,10 +14,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class Controller {
 
+	Logger logger = LogManager.getLogger(Controller.class);
+	
 	@RequestMapping(value="/test" ,method= RequestMethod.GET)
-    public ResponseEntity<String> test() {
+    public ResponseEntity<String> test(@RequestHeader(value = "User-Agent") String userAgent) {
         try {
-            return new ResponseEntity<>("", HttpStatus.OK);
+        	logger.error("userAgent: " + userAgent);
+            return new ResponseEntity<>("response\n", HttpStatus.OK);
         } catch (Exception e) {
             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
